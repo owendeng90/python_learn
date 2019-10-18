a5a13dc86297a34176ec0763d36e1eb46cea9ceb
owendeng90 <owendeng900@gmail.com>
2019-10-18 22:27:08 +0800

    commit

---
+++ 生成验证码图片/20191018_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/10/18 22:23
+# @Author  : owen
+# @Site    : 
+# @File    : 20191018_2.py
+# @Software: PyCharm Community Edition
+# @Code thought
